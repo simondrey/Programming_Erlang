6f17e7fa583b8705f89589271695d089920ee5c1
szydre <szymon.drejewicz@hotmail.com>
2016-10-17 18:57:40 +0200

    Chapter02 exercises

---
+++ Chapter02/exercise2.erl
@@ -0,0 +1,14 @@
+Erlang/OTP 18 [erts-7.1] [source] [64-bit] [smp:4:4] [async-threads:10] [hipe] [kernel-poll:false] [dtrace]
+
+Eshell V7.1  (abort with ^G)
+1> 2+2.
+4
+2> 1=1.
+1
+3> 1=2.
+** exception error: no match of right hand side value 2
+4> 2*10.
+20
+5> "Szymon".
+"Szymon"
+6> 
